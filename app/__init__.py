18eb35854ec3dcf1485518c1b74c1a25a4544037
Tami Takada <ttakada20@stuy.edu>
2021-12-10 08:40:47 -0500

    added login route

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,8 @@
 from flask import Flask, render_template, request, url_for
 from listify import *
 
- 
+@app.route('/login', methods=['GET', 'POST'])
+def display_login():
+    return render_template(
+        'login.html'
+    )
