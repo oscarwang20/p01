8837a105f6484ba9054b9d7beecea09a7e40d893
rengzhengcodes <rzhengcontacts@gmail.com>
2021-12-10 08:31:04 -0500

    got random words working

--- app/api_calls.py
+++ app/api_calls.py
@@ -1,8 +1,22 @@
+import json
 import urllib3
 http = urllib3.PoolManager() # general requests manager that the rest of the functions will use.
 
-def get_random_word(number: int = 1) -> str:
-	r = http.request('GET', f'https://random-words-api.vercel.app/word?{number}')
-	print(r)
+def get_random_word(number: int = 1) -> list:
+	'''Get a random word.
 
-get_random_word()
+	Keyword arguments:
+	number -- the number of expected returns (default 1)
+
+	return a list of random words'''
+	words = list()
+
+	for i in range(number):
+		r = http.request('GET', f'https://random-words-api.vercel.app/word') # request
+		r = r.data # extracts text from http request
+		r = json.loads(r) #turns json to dict.
+		words.append(r[0]['word']) # extracts only the word data
+
+	return words
+
+print(get_random_word(2))
