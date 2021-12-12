3d0ba47a0fa45c18173699df7f320f1e3419e7df
rengzhengcodes <rzhengcontacts@gmail.com>
2021-12-11 19:54:42 -0500

    encoding error only occurs when printing

--- app/api_calls.py
+++ app/api_calls.py
@@ -1,3 +1,4 @@
+import codecs
 import json
 import urllib3
 http = urllib3.PoolManager() # general requests manager that the rest of the functions will use.
@@ -16,7 +17,7 @@ def get_random_word(number: int = 1) -> list:
 
 	for i in range(number):
 		r = http.request('GET', f'https://random-words-api.vercel.app/word') # request
-		r = r.data.decode('utf8') #data from request
+		r = r.data #data from request
 		r = json.loads(r) #loads data into json
 		words.append(r[0]['word']) # extracts only the word data
 
@@ -33,9 +34,10 @@ def get_word_definition(word: str) -> dict:
 
 	r = http.request('GET', f'https://dictionaryapi.com/api/v3/references/collegiate/json/{word}?key={keys["webster_dictionary"]}') # requests word from dictionary api with provisioned key
 
-	r = r.data.decode('utf-8') #gets json from response
+	r = r.data #gets json from response
 	r = json.loads(r)
+	print(r)
 	return r
 
 print(get_random_word(2))
-print(get_word_definition("apple"))
+get_word_definition("apple")
