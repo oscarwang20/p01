f41c789a79576bb7d132f31f0ec87d13afa7932d
Oscar <oscarwang22396@gmail.com>
2021-12-13 08:40:03 -0500

    database stuff

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,19 @@
 from flask import Flask, render_template, request, url_for
-from listify import *
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def display_login():
     return render_template(
         'login.html'
     )
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
