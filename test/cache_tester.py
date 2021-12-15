dc663fc566d42406f6f633e08bc8014923f8b771
rengzhengcodes <rzhengcontacts@gmail.com>
2021-12-15 08:34:53 -0500

    made init load

--- test/cache_tester.py
+++ test/cache_tester.py
@@ -3,5 +3,8 @@ import sys
 from os import path
 #adds the repo to the sys paths. Gets abs path, gets parent directory, then the parent directory of that to get repo directory.
 sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+sys.path.append(path.dirname(path.dirname(path.abspath(__file__)))+ "/app")
 #imports from ../app/get_links.py
-from app.get_links import Cache_manager
+from app import get_links
+
+get_links.get_definition("apple")
