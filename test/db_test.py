8a6784957f174fbf5a5a9156f0d3de9921b8faa3
rengzhengcodes <rzhengcontacts@gmail.com>
2021-12-20 08:08:46 -0500

    moved test file to test folder

--- test/db_test.py
+++ test/db_test.py
@@ -1,3 +1,9 @@
+import sys
+from os import path
+#adds the repo to the sys paths. Gets abs path, gets parent directory, then the parent directory of that to get repo directory.
+sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+sys.path.append(path.dirname(path.dirname(path.abspath(__file__)))+ "/app")
+
 from db_builder import *
 
 def show_user_exists(username):
